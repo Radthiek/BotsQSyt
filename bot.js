d0089139dc73db8b23efe38723fe94a5e84401b5
Radthiek <44319561+Radthiek@users.noreply.github.com>
2018-11-01 10:18:51 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,151 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('ready', () => {
+  console.log('?[??????????????????????????????????]?');
+  console.log('')
+  console.log('            ?[????????????]?')
+  console.log('              Bot Is Online')
+  console.log('            ?[????????????]?')
+  console.log('')
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log('')
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log('')
+  console.log('?[????????????????????????????????????]?')
+});
+
+client.on("ready", async  => {
+setInterval(function(){
+client.channels.find('id', '507463091299155968').setName("༺");
+client.channels.find('id', '507463091299155968').setName("༺W");
+client.channels.find('id', '507463091299155968').setName("༺We");
+client.channels.find('id', '507463091299155968').setName("༺Wel");
+client.channels.find('id', '507463091299155968').setName("༺Welc");
+client.channels.find('id', '507463091299155968').setName("༺Welco");
+client.channels.find('id', '507463091299155968').setName("༺Welcom");
+client.channels.find('id', '507463091299155968').setName("༺Welcome");
+client.channels.find('id', '507463091299155968').setName("༺Welcome T");
+client.channels.find('id', '507463091299155968').setName("༺Welcome To");
+client.channels.find('id', '507463091299155968').setName("༺Welcome To R");
+client.channels.find('id', '507463091299155968').setName("༺Welcome To Ra");
+client.channels.find('id', '507463091299155968').setName("༺Welcome To Rad༻");
+client.channels.find('id', '507463091299155968').setName("༺Welcome To Rad༻🌹");
+  }, 3000);
+});
+
+
+   client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+        if (message.author.bot) return
+        message.channel.createInvite({
+        thing: true,
+        maxUses: 1,
+        maxAge: 1,
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+          .setDescription(" تم ارسال الرابط في الخاص :link: ")
+           .setAuthor(client.user.username, client.user.avatarURL)
+                 .setAuthor(client.user.username, client.user.avatarURL)
+                .setFooter('طلب بواسطة: ' + message.author.tag)
+
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+
+    .setDescription(" مدة الرابط :  24 ساعه فقط  عدد استخدامات الرابط : 5 ")
+      message.author.sendEmbed(Embed11)
+    }
+});
+
+             var prefix = ".";
+	client.on('message', message => {
+		if(!message.channel.guild) return;
+ if(message.content.startsWith(prefix + 'bc')) {
+ if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+ if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+ let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+ let BcList = new Discord.RichEmbed()
+ .setThumbnail(message.author.avatarURL)
+ .setAuthor(`محتوى الرساله ${args}`)
+ .setDescription(`برودكاست بـ امبد 📝\nبرودكاست بدون امبد✏ \nلديك دقيقه للأختيار قبل الغاء البرودكاست`)
+ if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(BcList).then(msg => {
+ msg.react('📝')
+ .then(() => msg.react('✏'))
+ .then(() =>msg.react('📝'))
+	
+ let EmbedBcFilter = (reaction, user) => reaction.emoji.name === '📝' && user.id === message.author.id;
+ let NormalBcFilter = (reaction, user) => reaction.emoji.name === '✏' && user.id === message.author.id;
+	
+ let EmbedBc = msg.createReactionCollector(EmbedBcFilter, { time: 60000 });
+ let NormalBc = msg.createReactionCollector(NormalBcFilter, { time: 60000 });
+	
+ EmbedBc.on("collect", r => {
+ message.channel.send(`:ballot_box_with_check: تم ارسال الرساله بنجاح`).then(m => m.delete(5000));
+ message.guild.members.forEach(m => {
+ var bc = new
+ Discord.RichEmbed()
+ .setColor('RANDOM')
+	 .setTitle('`-Broadcast-`')
+ .setAuthor(`Server : ${message.guild.name}`)
+ .setFooter(`Sender : ${message.author.username}`)
+ .setDescription(`Message : ${args}`)
+ .setThumbnail(message.author.avatarURL)
+ m.send({ embed: bc })
+ msg.delete();
+ })
+ })
+ NormalBc.on("collect", r => {
+	 message.channel.send(`:ballot_box_with_check: تم ارسال الرساله بنجاح`).then(m => m.delete(5000));
+ message.guild.members.forEach(m => {
+ m.send(args);
+ msg.delete();
+ })
+ })
+ })
+ }
+ });
+
+
+client.on('message', message => { 
+           if (message.content.startsWith(prefix + "id")) {
+     var args = message.content.split(" ").slice(1);
+     let user = message.mentions.users.first();
+     var men = message.mentions.users.first();
+        var heg;
+        if(men) {
+            heg = men
+        } else {
+            heg = message.author
+        }
+      var mentionned = message.mentions.members.first();
+         var h;
+        if(mentionned) {
+            h = mentionned
+        } else {
+            h = message.member
+        }
+               moment.locale('ar+TN');
+      var id = new  Discord.RichEmbed()
+      .setAuthor(message.author.username, message.author.avatarURL) 
+    .setColor("#707070")
+    .addField(': دخولك لديسكورد قبل', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true) 
+    .addField(': انضمامك لسيرفر قبل', `${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n \`${moment(h.joinedAt).fromNow()}\``, true)               
+    .setFooter(`arab bot`, 'https://images+ext+2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B+W_128x128.gif')                                 
+    .setThumbnail(heg.avatarURL);
+    message.channel.send(id)
+}       });
+
+
+
+client.login('NTA3NDgyOTg0NzY1NzE4NTM5.DrxWEg.JOsQdbpDN_xuta6NgjPU6yhaLas');
